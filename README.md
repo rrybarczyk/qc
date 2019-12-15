717a1aa37c4b20bec9814401fdf436789ec7fac4
RJ Rybarczyk <rj@rybar.tech>
2019-12-15 20:29:17 +0000

    RPN add, sub, mul, div, :add, :sub, :mul, :div

--- README.md
+++ README.md
@@ -1,17 +1,224 @@
-# Quick Calc
+# qc
 
+```
+$ cargo install qc
+```
+
+qc (short for quick calc) is an enhanced Reverse Polish notation (RPN) command line tool designed to assist with quick and dirty calcs.
+
+
+qc offers enhancements to a traditional RPN calculator including the use of a `:` before all operators that pops all items before it off the stack, performs the operation, and pushes the result onto the stack. Examples are included below.
+
+## Examples
+### Traditional RPN Capabilities
+#### Addition
+```
+$ qc 1 2 add
+Script                  Stack                   Details 
+                                        
+                        [ ]
+1                       [ 1 ]
+2                       [ 1 2 ]
+add                     [ 3 ]                   1 + 2
+
+
+$ qc 1 2 3 add add
+Script                  Stack                   Details 
+                        [ ]
+1                       [ 1 ]
+2                       [ 1 2 ]
+3                       [ 1 2 3 ]
+add                     [ 1 5 ]                  2 + 3
+add                     [ 6 ]                    1 + 5
+```
+
+#### Subtraction
+```
+$ qc 1 2 sub            
+Script                  Stack                   Details 
+                        [ ]
+1                       [ 1 ]
+2                       [ 1 2 ]
+sub                     [ -1 ]                  1 - 2
+
+$ qc 1 2 3 sub sub
+Script                  Stack                   Details 
+                        [ ]
+1                       [ 1 ]
+2                       [ 1 2 ]
+3                       [ 1 2 3 ]
+sub                     [ 1 -1 ]                2 - 3
+sub                     [ 2 ]                   1 - -1
+```
+
+#### Multiplication
+```
+$ qc 1 2 mul
+Script                  Stack                   Details 
+                                        
+                        [ ]
+1                       [ 1 ]
+2                       [ 1 2 ]
+mul                     [ 2 ]                   1 * 2
+
+
+$ qc 1 2 3 mul mul
+Script                  Stack                   Details 
+                        [ ]
+1                       [ 1 ]
+2                       [ 1 2 ]
+3                       [ 1 2 3 ]
+mul                     [ 1 6 ]                 2 * 3
+mul                     [ 6 ]                   1 * 6
+```
+
+#### Division
+```
+$ qc 3 9 div
+Script                  Stack                   Details 
+                                        
+                        [ ]
+3                       [ 3 ]
+9                       [ 3 9 ]
+div                     [ 0.5 ]                 3 / 9
+
+
+$ qc 1 2 3 div div
+Script                  Stack                   Details 
+                        [ ]
+1                       [ 1 ]
+2                       [ 1 2 ]
+3                       [ 1 2 3 ]
+div                     [ 1 6 ]                 2 * 3
+div                     [ 6 ]                   6 * 1
+```
+
+#### Miscellaneous
+
+```
+$ qc 4 7 9 add 2 8 mul mul mul
+Script                  Stack                   Details 
+4                       [ 4 ]
+7                       [ 4 7 ]
+9                       [ 4 7 9 ]
+add                     [ 4 16 ]                7 + 9
+2                       [ 4 16 2]
+8                       [ 4 16 2 8 ]
+mul                     [ 4 16 16 ]             2 * 8
+mul                     [ 4 256 ]               16 * 16
+mul                     [ 1024 ]                4 * 256
+```
+
+```
+$ qc 4 7 9 add 2 8 mul mul 4 div sub
+Script                  Stack                   Details 
+4                       [ 4 ]
+7                       [ 4 7 ]
+9                       [ 4 7 9 ]
+add                     [ 4 16 ]                7 + 9
+2                       [ 4 16 2]
+8                       [ 4 16 2 8 ]
+mul                     [ 4 16 16 ]             2 * 8
+mul                     [ 4 256 ]               16 * 16
+4                       [ 4 256 4]
+div                     [ 4 64 ]                256 / 4
+sub                     [ -60 ]                 4 - 64
+```
+
+```
+$ qc 4 7 9 add add  2 3 5 mul mul mul 1 1 sub sub 20 5 div div .
+Script                  Stack                   Details 
+4                       [ 4 ]
+7                       [ 4 7 ]
+9                       [ 4 7 9 ]
+add                     [ 4 16 ]                7 + 9
+add                     [ 20 ]                  4 + 16
+2                       [ 20 2 ]
+3                       [ 20 2 3 ]
+5                       [ 20 2 3 5 ]
+mul                     [ 20 2 15 ]             3 * 5
+mul                     [ 20 30 ]               2 * 15
+mul                     [ 600 ]                 20 * 30
+1                       [ 600 1 ]
+1                       [ 600 1 1 ]
+sub                     [ 600 0 ]               600 - 1
+sub                     [ 600 ]                 600 - 0
+20                      [ 600 20 ]
+5                       [ 600 20 5 ]
+div                     [ 600 4 ]               20 / 5
+div                     [ 150 ]                 600 / 4
+```
+
+### Enhanced RPN Capabilities
+```
+$ qc 4 7 9 :add 2 3 5 :mul 1 1 :sub 20 5 :div .
+Script                  Stack                   Details 
+4                       [ 4 ]
+7                       [ 4 7 ]
+9                       [ 4 7 9 ]
+:add                    [ 20 ]                  4 + 7 + 9
+2                       [ 20 2 ]
+3                       [ 20 2 3 ]
+5                       [ 20 2 3 5 ]
+:mul                    [ 600 ]                 20 * 2 * 3 * 5
+1                       [ 600 1 ]
+1                       [ 600 1 1 ]
+: sub                   [ 600 ]                 600 - (1 - 1)
+20                      [ 600 20 ]
+5                       [ 600 20 5 ]
+:div                    [ 150 ]                 600 / (20 / 5)
+```
+
+### Printing
+Pop of the top item on the stack and print it.
+```
+$ qc 1 2 3 add add .
+> 6
+
+$ qc 8 32 div .
+> 4
+
+$ qc 1 2 3 :add 4 5 :mul .
+> 120
+
+```
+
+## TODO
+- [ ] Hexadecimal, octal, and binary formatted print
+- [ ] Endianness swapping on all stack items plus formatted print
+```
+$ qc 0xe803000000000000 0xd007000000000000 :endian
+> dec: 1000     hex: 0x00000000000003e8     oct: o1750      bin: b01111101000
+> dec: 2000     hex: 0x07d0000000000000     oct: o3720      bin: b11111010000
+```
+- [ ] Hashing on all stack items
+```
+qc 0x00000000000003e8 0x00000000000007d0 :sha256d
+```
+- [ ] Concatenate all stack items and hash
+```
+qc 0x00000000000003e8 0x00000000000007d0 :cat sha256d
+```
+
+
+## Character Guide
+Safe
 ```
 SPACE
 ABCDEFGHIJKLMNOPQRSTUVWXYZ
 abcdefghijklmnopqrstuvwxyz
 0123456789
 @[]^_{}+,-./:
+```
 
-Dangerous:
+Dangerous
+```
 \`|~<>"'#&()*;? TAB
+```
 
 
-Questionable:
+Questionable
+```
 {}!%=
 ~ - can use if not first character in a line
 $ - on its own can be an operator, but not in fish
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,287 @@
-fn main() { }
+fn run(args: impl IntoIterator<Item = impl AsRef<str>>) -> Vec<f64> {
+    let mut stack = Vec::new();
+    for arg in args {
+        let arg = arg.as_ref();
+        match arg {
+            "add" => add(&mut stack),
+            "sub" => sub(&mut stack),
+            "mul" => mul(&mut stack),
+            "div" => div(&mut stack),
+            ":add" => add_all(&mut stack),
+            ":sub" => sub_all(&mut stack),
+            ":mul" => mul_all(&mut stack),
+            ":div" => div_all(&mut stack),
+            "." => pop_print(&mut stack),
+            _ => num(&mut stack, arg),
+        }
+    }
+    stack
+}
+
+fn main() {
+    let mut args = std::env::args().collect::<Vec<String>>();
+    args.remove(0);
+    dbg!("{}", &args);
+    run(&args);
+}
+
+fn pop_print(stack: &mut Vec<f64>) {
+    println!("{}", stack.pop().unwrap());
+}
+
+/// Pops top two items off the stack, adds them, and pushes the sum on the stack.
+fn add(stack: &mut Vec<f64>) {
+    let a = stack.pop().unwrap();
+    let b = stack.pop().unwrap();
+    let sum = b + a;
+    stack.push(sum);
+}
+
+/// Pops top two items off the stack, subtracts them, and pushes the difference on the stack.
+fn sub(stack: &mut Vec<f64>) {
+    let a = stack.pop().unwrap();
+    let b = stack.pop().unwrap();
+    let difference = b - a;
+    stack.push(difference);
+}
+
+/// Pops top two items off the stack, multiplies them, and pushes the product on the stack.
+fn mul(stack: &mut Vec<f64>) {
+    let a = stack.pop().unwrap();
+    let b = stack.pop().unwrap();
+    let product = b * a;
+    stack.push(product);
+}
+
+/// Pops top two items off the stack, divides them, and pushes the quotient on the stack.
+fn div(stack: &mut Vec<f64>) {
+    let a = stack.pop().unwrap();
+    let b = stack.pop().unwrap();
+    let quotient = b / a;
+    stack.push(quotient);
+}
+
+/// Pops all items before `:add` off the stack, adds them, and pushes the sum on the stack.
+fn add_all(stack: &mut Vec<f64>) {
+    let a = stack.pop().unwrap();
+    let b = stack.pop().unwrap();
+    let mut sum = b + a;
+
+    while !stack.is_empty() {
+        sum += stack.pop().unwrap();
+    }
+    stack.push(sum);
+}
+
+/// Pops all items before `:sub` off the stack, subtracts them, and pushes the difference on the stack.
+fn sub_all(stack: &mut Vec<f64>) {
+    let a = stack.pop().unwrap();
+    let b = stack.pop().unwrap();
+    let mut difference = b - a;
+
+    while !stack.is_empty() {
+        difference = stack.pop().unwrap() - difference;
+    }
+    stack.push(difference);
+}
+
+/// Pops all items before `:mul` off the stack, multiplies them, and pushes the product on the stack.
+fn mul_all(stack: &mut Vec<f64>) {
+    let a = stack.pop().unwrap();
+    let b = stack.pop().unwrap();
+    let mut product = b * a;
+
+    while !stack.is_empty() {
+        product *= stack.pop().unwrap();
+    }
+    stack.push(product);
+}
+
+/// Pops all items before `:div` off the stack, divides them, and pushes the quotient on the stack.
+fn div_all(stack: &mut Vec<f64>) {
+    let a = stack.pop().unwrap();
+    let b = stack.pop().unwrap();
+    let mut quotient = b / a;
+
+    while !stack.is_empty() {
+        quotient = stack.pop().unwrap() / quotient;
+    }
+    stack.push(quotient);
+}
+
+/// Parse arg as a number and push it onto the stack
+fn num(stack: &mut Vec<f64>, arg: &str) {
+    stack.push(arg.parse().unwrap());
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    /// qc 1 2 add
+    fn two_args_add() {
+        let args = &["1", "2", "add"];
+        let have = run(args);
+        let want = vec![3.0];
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    /// qc 1 2 3 add
+    fn three_args_add() {
+        let args = &["1", "2", "3", "add"];
+        let have = run(args);
+        let want = vec![1.0, 5.0];
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    /// qc 1 2 sub
+    fn two_args_sub() {
+        let args = &["1", "2", "sub"];
+        let have = run(args);
+        let want = vec![-1.0];
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    /// qc 1 2 3 sub
+    fn three_args_sub() {
+        let args = &["1", "2", "3", "sub"];
+        let have = run(args);
+        let want = vec![1.0, -1.0];
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    /// qc 1 2 mul
+    fn two_args_mul() {
+        let args = &["1", "2", "mul"];
+        let have = run(args);
+        let want = vec![2.0];
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    /// qc 1 2 3 mul
+    fn three_args_mul() {
+        let args = &["1", "2", "3", "mul"];
+        let have = run(args);
+        let want = vec![1.0, 6.0];
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    /// qc 9 3 div
+    fn two_args_div() {
+        let args = &["9", "3", "div"];
+        let have = run(args);
+        let want = vec![3.0];
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    /// qc 1 6 2 div
+    fn three_args_div() {
+        let args = &["1", "6", "2", "div"];
+        let have = run(args);
+        let want = vec![1.0, 3.0];
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    /// qc 1 2 :add
+    fn two_args_add_all() {
+        let args = &["1", "2", ":add"];
+        let have = run(args);
+        let want = vec![3.0];
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    /// qc 1 2 3 :add
+    fn three_args_add_all() {
+        let args = &["1", "2", "3", ":add"];
+        let have = run(args);
+        let want = vec![6.0];
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    /// qc 1 2 :sub
+    fn two_args_sub_all() {
+        let args = &["1", "2", ":sub"];
+        let have = run(args);
+        let want = vec![-1.0];
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    /// qc 1 2 3 :sub
+    fn three_args_sub_all() {
+        let args = &["1", "2", "3", ":sub"];
+        let have = run(args);
+        let want = vec![2.0];
+        assert_eq!(have, want);
+
+        let args = &["6", "2", "1", ":sub"];
+        let have = run(args);
+        let want = vec![5.0];
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    /// qc 1 2 mul
+    fn two_args_mul_all() {
+        let args = &["1", "2", ":mul"];
+        let have = run(args);
+        let want = vec![2.0];
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    /// qc 1 2 3 :mul
+    fn three_args_mul_all() {
+        let args = &["1", "2", "3", ":mul"];
+        let have = run(args);
+        let want = vec![6.0];
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    /// qc 9 3 :div
+    fn two_args_div_all() {
+        let args = &["9", "3", ":div"];
+        let have = run(args);
+        let want = vec![3.0];
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    /// qc 1 6 2 :div
+    fn three_args_div_all() {
+        let args = &["6", "2", "1", ":div"];
+        let have = run(args);
+        let want = vec![3.0];
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    /// qc 4 7 9 add 2 8 mul
+    fn three_add_two_mul() {
+        let args = &["4", "7", "9", "add", "2", "8", "mul"];
+        let have = run(args);
+        let want = vec![4.0, 16.0, 16.0];
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    /// qc 4 7 9 add add 2 8 mul mul
+    fn three_add_add_two_mul_mul() {
+        let args = &["4", "7", "9", "add", "add", "2", "8", "mul", "mul"];
+        let have = run(args);
+        let want = vec![320.0];
+        assert_eq!(have, want);
     }
 }
